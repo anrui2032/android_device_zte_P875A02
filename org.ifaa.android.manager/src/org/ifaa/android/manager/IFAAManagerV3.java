823e99aa090c355b835b667f9e5dde24487e493b
dianlujitao <dianlujitao@lineageos.org>
2020-01-09 20:46:29 +0800

    P875A02: Add ifaa apis to unrestricted greylist
    Change-Id: If33a18edaf6083281265ce94fd143d72b4bb4fb8

--- org.ifaa.android.manager/src/org/ifaa/android/manager/IFAAManagerV3.java
+++ org.ifaa.android.manager/src/org/ifaa/android/manager/IFAAManagerV3.java
@@ -1,12 +1,16 @@
 package org.ifaa.android.manager;
 
+import android.annotation.UnsupportedAppUsage;
+
 public abstract class IFAAManagerV3 extends IFAAManagerV2 {
     public static final String KEY_FINGERPRINT_FULLVIEW = "org.ifaa.ext.key.CUSTOM_VIEW";
     public static final String KEY_GET_SENSOR_LOCATION = "org.ifaa.ext.key.GET_SENSOR_LOCATION";
     public static final String VALUE_FINGERPRINT_DISABLE = "disable";
     public static final String VLAUE_FINGERPRINT_ENABLE = "enable";
 
+    @UnsupportedAppUsage
     public abstract String getExtInfo(int i, String str);
 
+    @UnsupportedAppUsage
     public abstract void setExtInfo(int i, String str, String str2);
 }

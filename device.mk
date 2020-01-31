143fdbe244ed5b9081d5ccfe09e5a8a00b023482
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2020-01-31 11:34:15 -0800

    P875A02: Add UClamp support for lahaina
    Sched Tune is deprecated in 5.4 Kernels and is replaced with
    UClamp. As Android releases has to support earlier Kernel versions
    like 4.14, UClamp isn't made default yet.
    
    Change-Id: If6f4ae0812ae456089273514de58bb0654bb08c7

--- device.mk
+++ device.mk
@@ -313,6 +313,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 

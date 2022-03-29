9206c24581be8a69305e45787cf5c636fd990e95
LuK1337 <priv.luk@gmail.com>
2022-03-30 00:30:07 +0200

    P875A02: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,7 +100,7 @@ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_RAMDISK_USE_LZ4 := true
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
-TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
+TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig vendor/debugfs.config
 TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/zte/sm8350
 

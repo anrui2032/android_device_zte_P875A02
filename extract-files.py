788810cb460de9272fee0435bd0d6c82d590218f
Michael Bestas <mkbestas@lineageos.org>
2024-11-03 07:33:51 +0200

    P875A02: Switch to python extract-utils
    Change-Id: Ida4b83459d9739ac880b26a91df974e1db8423e9

---
+++ extract-files.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/zte/P875A02',
+    'hardware/qcom-caf/sm8350',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'libskia',
+        'vendor.qti.diaghal@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+    (
+        'libwpa_client',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/wifi/wlan/WCNSS_qcom_cfg.ini': blob_fixup()
+        .regex_replace('\nEND', '\nread_mac_addr_from_mac_file=1\nEND'),
+    (
+        'vendor/etc/media_codecs.xml',
+        'vendor/etc/media_codecs_lahaina_vendor.xml'
+    ): blob_fixup()
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', '')
+        .regex_replace('.*media_codecs_google_c2.*\n', ''),
+    'vendor/lib64/libFNVfbEngineHAL.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/libZEffectLib.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_lockPlanes')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/hw/fingerprint.lahaina.so': blob_fixup()
+        .binary_regex_replace(b'libfingerprint.default.so\x00', b'fingerprint.lahaina.so\x00\x00\x00\x00')
+        .binary_regex_replace(b'fingerprint.gf95xx\x00', b'fingerprint\x00\x00\x00\x00\x00\x00\x00\x00'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'P875A02',
+    'zte',
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()

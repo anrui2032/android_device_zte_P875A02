3079e2a5e54b7659320b8455655bcb5681fecd6a
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    P875A02: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -237,6 +237,11 @@ PRODUCT_PACKAGES += \
     ZTESystemUIRes
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power

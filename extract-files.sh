a44c3f916c9f970bede8b6c730e8eea0e223a8fb
Michael Bestas <mkbestas@lineageos.org>
2025-01-24 17:20:58 +0200

    P875A02: Clear LIBNATIVEWINDOW symbol versions for camera blobs
    Change-Id: I1c0d85c5a5fa2dbd380eb50115ca9ad729c6f0b3
    Signed-off-by: anrui2032 <anrui2032@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,23 @@ function blob_fixup() {
     [ "$2" = "" ] && return 0
         sed -i '/^END$/i read_mac_addr_from_mac_file=1' "${2}"
         ;;
+    vendor/lib64/libFNVfbEngineHAL.so)
+    [ "$2" = "" ] && return 0
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_allocate" "${2}"
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_describe" "${2}"
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_lock" "${2}"
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_release" "${2}"
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_unlock" "${2}"
+        ;;
+    vendor/lib64/libZEffectLib.so)
+    [ "$2" = "" ] && return 0
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_allocate" "${2}"
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_describe" "${2}"
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_lock" "${2}"
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_lockPlanes" "${2}"
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_release" "${2}"
+        "${PATCHELF_0_17_2}" --clear-symbol-version "AHardwareBuffer_unlock" "${2}"
+        ;;
     # Change soname for fingerprint.gf95xx.default.so.
     vendor/lib64/hw/fingerprint.lahaina.so)
     [ "$2" = "" ] && return 0

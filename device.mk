e7c43407f4f3b6a2b7e16c41edf524ddb00a3138
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    P875A02: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- device.mk
+++ device.mk
@@ -217,6 +217,9 @@ PRODUCT_PACKAGES += \
     init.vendor.usb.rc \
     ueventd.qcom.rc
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Keymaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml

0611ce15aced7d5286ed615fa9989e857376458a
anrui2032 <anrui2032@gmail.com>
2026-04-07 03:02:12 +0800

    P875A02: Build vendor variant libutilscallstack
    Change-Id: I78fbb6b72ab69a3c6b293c61137533bd9493b2e5

--- device.mk
+++ device.mk
@@ -118,6 +118,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libutilscallstack.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 

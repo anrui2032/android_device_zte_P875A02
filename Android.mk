08b6310bd572abd1280009a03baac3d86320059f
anrui2032 <anrui2032@gmail.com>
2026-06-23 02:52:01 +0800

    P875A02: wifi-mac-generator: Adapt to read the mac from logdump
    Change-Id: I7ec1a9f85779dd71d5006a30c83a16b8ebacf7ca

--- Android.mk
+++ Android.mk
@@ -75,7 +75,7 @@ WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld
 $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating WiFi firmware symlinks: $@"
 	@mkdir -p $@
-	$(hide) ln -sf /mnt/vendor/persist/wlan_mac.bin $@/wlan_mac.bin
+	$(hide) ln -sf /logdump/wlan_mac.bin $@/wlan_mac.bin
 	$(hide) ln -sf /vendor/etc/wifi/wlan/WCNSS_qcom_cfg.ini $@/WCNSS_qcom_cfg.ini
 
 ALL_DEFAULT_INSTALLED_MODULES += \

7a587cea85d5fe616b64b23cc7b506fc956d69c9
anrui2032 <anrui2032@gmail.com>
2026-06-23 02:52:01 +0800

    P875A02: wifi-mac-generator: Adapt to read the mac from logdump
    Change-Id: I7ec1a9f85779dd71d5006a30c83a16b8ebacf7ca

--- wifi-mac-generator/wifi-mac-generator.sh
+++ wifi-mac-generator/wifi-mac-generator.sh
@@ -1,21 +1,21 @@
 #!/vendor/bin/sh
 
 # data format:
-# hex binary file containing the mac
-WLAN_MAC_HEX_PATH="/mnt/vendor/persist/wlan_bt/wlan.mac"
+# ASCII text file containing the mac
+WIFI_MAC_ADDR_PATH="/mnt/vendor/persist/wifimac.dat"
 
 # data format:
 # Intf0MacAddress=00AA00BB00CC
 # Intf1MacAddress=00AA00BB00CD
 # END
-WLAN_MAC_PERSIST_PATH="/mnt/vendor/persist/wlan_mac.bin"
+WLAN_MAC_LOGDUMP_PATH="/logdump/wlan_mac.bin"
 
-if [ ! -s "${WLAN_MAC_HEX_PATH}" ]; then
+if [ ! -s "${WIFI_MAC_ADDR_PATH}" ]; then
     exit
 fi
 
 # Read the mac from persist
-raw_mac=$(xxd -p $WLAN_MAC_HEX_PATH)
+raw_mac=$(cat "$WIFI_MAC_ADDR_PATH" | sed 's/^wifiaddr://; s/0x//g; s/ //g')
 
 # Convert to decimal
 dec_mac=$(printf "%d" "0x$raw_mac")
@@ -32,6 +32,6 @@ dec_mac=$(expr $dec_mac + 1 )
 second_mac=$(printf "%012X" "$dec_mac")
 
 # Write the MACs
-echo "Intf0MacAddress=${first_mac}" > "${WLAN_MAC_PERSIST_PATH}"
-echo "Intf1MacAddress=${second_mac}" >> "${WLAN_MAC_PERSIST_PATH}"
-echo "END" >> "${WLAN_MAC_PERSIST_PATH}"
+echo "Intf0MacAddress=${first_mac}" > "${WLAN_MAC_LOGDUMP_PATH}"
+echo "Intf1MacAddress=${second_mac}" >> "${WLAN_MAC_LOGDUMP_PATH}"
+echo "END" >> "${WLAN_MAC_LOGDUMP_PATH}"
